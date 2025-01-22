3ad221b8f80fb9106884e3d9ddb9b46a3c8969ae
nk-on <2nkoz@proton.me>
2025-01-21 23:18:47 -0800

    Feat:Adding header section with night and day mode switching

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+interface HeaderProps {
+  nightModeOff: boolean;
+  setNightModeOff: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function Header({ nightModeOff, setNightModeOff }: HeaderProps) {
+  return (
+    <div className={`w-[100%] flex justify-between items-center ${!nightModeOff && "text-[#FFF]"}`}>
+      <h1 className="font-bold text-[26px]">devfinder</h1>
+      <span className="flex gap-[5px] text-[13px]">
+        {nightModeOff ? "Dark" : "Light"}{" "}
+        <img
+          src={nightModeOff ? "src/assets/NightIcon.svg" : "src/assets/DayIcon.svg"}
+          alt="ModeIcon"
+          onClick={()=>{
+            setNightModeOff(!nightModeOff);
+          }}
+          className="cursor-pointer w-[20px] h-[20px]"
+        ></img>
+      </span>
+    </div>
+  );
+}
