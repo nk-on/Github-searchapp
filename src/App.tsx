a3bd596585d655c5b3664952ab121b2c6123e0d8
nk-on <2nkoz@proton.me>
2025-01-23 20:42:22 -0800

    Adding header to the inside App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Container from './components/Container'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Container from "./components/Container";
+import { SearchBar } from "./components/SearchBar";
 function App() {
-  const [nightModeOff,setNightModeOff] = useState<boolean>(true)
+  const [nightModeOff, setNightModeOff] = useState<boolean>(true);
   return (
     <>
-      <div className={`'w-[100vw] h-[100vh] flex justify-center items-center ${nightModeOff ? 'bg-[#F6F8FF]' : 'bg-[#141D2F]'}`}>
-        <div className='w-[90%] md:w-[730px] h-[600px]'>
-          <Header nightModeOff = {nightModeOff} setNightModeOff = {setNightModeOff}></Header>
-          <Container nightModeOff = {nightModeOff}></Container>
+      <div
+        className={`'w-[100vw] h-[100vh] flex justify-center items-center  ${
+          nightModeOff ? "bg-[#F6F8FF]" : "bg-[#141D2F]"
+        }`}
+      >
+        <div className="flex flex-col gap-[10px] w-[90%] md:w-[730px] h-[600px]">
+          <Header nightModeOff={nightModeOff} setNightModeOff={setNightModeOff}></Header>
+          <SearchBar nightModeOff={nightModeOff}></SearchBar>
+          <Container nightModeOff={nightModeOff}></Container>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
