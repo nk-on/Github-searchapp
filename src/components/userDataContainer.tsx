f233d79de40b3b9d7a7fd103b562986bdd802fc6
nk-on <johndoe@example.com>
2025-01-26 21:38:02 +0400

    Feat:adding not available text if user has not said data

--- src/components/userDataContainer.tsx
+++ src/components/userDataContainer.tsx
@@ -6,7 +6,7 @@ export default function UserDataContainer({imagePath,title}:userDataProp){
     return (
         <div className="flex">
         <img src={imagePath} alt="icon"></img>
-        <p className="text-[15px] text-[#4B6A9B]">{title}</p>
+        <div className="text-[15px] text-[#4B6A9B] flex gap-[5px]">{title ? title : <div className="text-[#a5b4cd]">Not available</div>}</div>
       </div>
     )
 }
