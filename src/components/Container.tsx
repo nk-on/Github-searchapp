9c42153fa006b023e4b732943bf9208b738b3b01
nk-on <2nkoz@proton.me>
2025-01-25 17:09:46 -0800

    Feat:adding user search feature

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,26 +1,30 @@
 import { useEffect, useState } from "react";
-import APIResponse from "../APIResponse";
 import Stats from "./Stats";
 import { statsData } from "../StatsData";
 import UserDataContainer from "./userDataContainer";
 import { userDataObj } from "../userData";
+import APIResponse from "../APIResponse";
 interface ContainerProps {
   nightModeOff: boolean;
+  setUserData: React.Dispatch<React.SetStateAction<APIResponse | null>>;
+  userData: APIResponse | null;
+  query: string;
 }
-export default function Container({ nightModeOff }: ContainerProps) {
-  console.log(userDataObj)
-  const query = "octocat";
-  const [userData, setUserData] = useState<APIResponse | null>(null);
-  const url = `https://api.github.com/users/${query}`;
-  // async function fetchData() {
-  //   const res = await fetch(url);
-  //   const data = await res.json();
-  //   console.log(data)
-  //   setUserData({ ...data });
-  // }
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+export default function Container({
+  nightModeOff,
+  userData,
+  setUserData,
+  query,
+}: ContainerProps) {
+  async function fetchData() {
+    const url = `https://api.github.com/users/${query}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setUserData({ ...data });
+  }
+  useEffect(() => {
+    fetchData();
+  }, [query]);
   return (
     <>
       <div
@@ -40,27 +44,26 @@ export default function Container({ nightModeOff }: ContainerProps) {
               <h1 className="text-[26px] text-[#2B3442] font-bold">{userData?.login}</h1>
               <p className="text-[#0079FF]">@{userData?.login}</p>
             </div>
-            <p className="text-[#697C9A]">joined {new Date('2022-10-20T11:18:49Z').getDay()} {new Date('2022-10-20T11:18:49Z').getFullYear()}</p>
+            <p className="text-[#697C9A]">
+              joined {new Date("2022-10-20T11:18:49Z").getDay()}{" "}
+              {new Date("2022-10-20T11:18:49Z").getFullYear()}
+            </p>
           </div>
           <div className="text-[#4B6A9B] w-[100%] md:w-[480px]">
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque
             volutpat mattis eros.
           </div>
           <div className="w-[90%] md:w-[480px] h-[85px] bg-[#F6F8FF] flex justify-between p-[20px]">
-            {
-              statsData.map((element)=>{
-                return <Stats  title = {element.title} amount = {element.data}  />
-              })
-            }
+            {statsData.map((element) => {
+              return <Stats title={element.title} amount={element.data} />;
+            })}
           </div>
           <div className="md:grid grid-cols-2 gap-[50px]">
-            {
-             userDataObj.map((element)=>{
+            {userDataObj.map((element) => {
               return (
                 <UserDataContainer imagePath={element.imagePath} title={element.title} />
-              )
-             })
-            }
+              );
+            })}
           </div>
         </div>
       </div>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,11 @@
+import { useRef } from "react";
 interface SearchBarProps {
   nightModeOff: boolean;
+  setQuery:React.Dispatch<React.SetStateAction<string>>
 }
-export function SearchBar({ nightModeOff }: SearchBarProps) {
+export function SearchBar({ nightModeOff ,setQuery }: SearchBarProps) {
   console.log(nightModeOff);
+  const searchQuery = useRef('')
   return (
     <div
       className={`w-[100%] h-[69px] px-[50px] flex  items-center rounded-[15px]  ${
@@ -12,12 +15,17 @@ export function SearchBar({ nightModeOff }: SearchBarProps) {
       <img src='src/assets/Combined Shape.svg' className="w-[24px] h-[24px]"></img>
       <input
         type="search"
+        onChange={(event)=>{
+          searchQuery.current = event.target.value;
+        }}
         className={`w-[90%] h-[100%] px-[10px] ${nightModeOff ? "bg-[#FEFEFE]" : "bg-[#1E2A47]"} ${
           nightModeOff ? "text-[#4B6A9B]" : "text-[#FFFFFF]"
         } `}
       ></input>
-      <button className={`w-[106px] h-[50px] rounded-[10px] bg-[#0079FF] text-[#FFF] font-700"
-        } `}>Search</button>
+      <button onClick = {()=>{
+         setQuery(searchQuery.current)
+      }} className={`w-[106px] h-[50px] rounded-[10px] bg-[#0079FF] text-[#FFF] font-700"
+        } ` }>Search</button>
     </div>
   );
 }
