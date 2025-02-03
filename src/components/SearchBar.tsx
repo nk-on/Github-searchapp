e61d824e033d36f5fa1ae2a14d05275092bf4db3
nk-on <johndoe@example.com>
2025-02-03 21:20:59 +0400

    Style:adding search icon inside input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,9 +9,6 @@ export function SearchBar({  setQuery, error }: SearchBarProps) {
     <div
       className='w-[100%] h-[69px] px-[50px] flex justify-evenly items-center rounded-[15px] bg-[#FEFEFE] dark:bg-[#1E2A47]'
     >
-      {/* <div className="flex justify-center items-center w-[5%]">
-        <img src="./assets/Combined Shape.svg" className="w-[24px] h-[24px]"></img>
-      </div> */}
       <input
         type="search"
         defaultValue={localStorage.getItem("query") || ""}
@@ -20,7 +17,7 @@ export function SearchBar({  setQuery, error }: SearchBarProps) {
           localStorage.setItem("query", searchQuery.current);
         }}
         placeholder="Search GitHub username…"
-        className={`w-[90%] h-[100%] px-[10px] bg-[url(./assets/Combined Shape.svg)] bg-[#FEFEFE] dark:bg-[#1E2A47] text-[#4B6A9B] dark:text-[#FFFFFF]`}
+        className='w-[90%] h-[100%] px-[10px] pl-[40px] bg-[#FEFEFE] dark:bg-[#1E2A47] text-[#4B6A9B] dark:text-[#FFFFFF] bg-[url(./assets/search.svg)] bg-no-repeat bg-center bg-left'
       ></input>
       {error && <div className="text-[15px] font-bold text-[#F74646]">No results</div>}
       <button
